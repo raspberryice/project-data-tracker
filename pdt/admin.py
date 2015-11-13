902b6a464cc47206e38a2d52457357d8c73442d4
septjew <zoey.lee13@gmail.com>
2015-11-13 17:57:00 +0000

    added models, admin site and .gitignore file

--- pdt/admin.py
+++ pdt/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Project,Phase,Iteration,Session,Defect,DevelopmentSession,DefectRemovalSession,ManagementSession
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(Phase)
+admin.site.register(Iteration)
+admin.site.register(Session)
+admin.site.register(Defect)
+admin.site.register(DevelopmentSession)
+admin.site.register(DefectRemovalSession)
+admin.site.register(ManagementSession)
--- pdt/models.py
+++ pdt/models.py
@@ -1,8 +1,78 @@
 from django.db import models
 
-# Create your models here.
-class Comment(models.Model):
-	name = models.CharField(max_length=30)
-	pub_date = models.DateTimeField('date published')
-	content = models.CharField(max_length=2000)
+
+class Project(models.Model):
+    name = models.CharField(max_length=30)
+    desc = models.CharField(max_length=200)
+    status = models.IntegerField
+    totalTime = models.IntegerField
+    totalSLOC = models.IntegerField
+    totalDefects =models.IntegerField
+
+    def __str__ (self):
+        return self.name
+
+
+class Phase(models.Model):
+    PHASE_NAMES = (
+        (1,'Inception'),
+        (2,'Elaboration'),
+        (3,'Construction'),
+        (4,'Migration')
+    )
+    project = models.ForeignKey(Project)
+    no = models.IntegerField(choices=PHASE_NAMES,default=1)
+    status = models.IntegerField
+    totalTime = models.IntegerField
+    totalSLOC = models.IntegerField
+    totalDefects = models.IntegerField
+    def __str__(self):
+       return self.no
+
+
+class Iteration(models.Model):
+    phase = models.ForeignKey(Phase)
+    no= models.IntegerField
+    status = models.IntegerField
+    totalTime = models.IntegerField
+    totalSLOC = models.IntegerField
+    totalDefects = models.IntegerField
+    def __str__(self):
+        return self.no
+
+
+class Session(models.Model):
+    iteration = models.ForeignKey(Iteration)
+
+
+
+class DevelopmentSession(Session):
+    SLOC = models.IntegerField
+    def getSLOC (self,newSLOC):
+        self.SLOC = newSLOC
+
+
+class ManagementSession(Session):
+    {
+
+    }
+
+
+class DefectRemovalSession(Session):
+    defectno = models.IntegerField
+
+
+class Defect(models.Model):
+    session=models.ForeignKey(DefectRemovalSession)
+    type = models.IntegerField
+    iterationInjected= models.ForeignKey(Iteration,related_name='injected')
+    iterationRemoved = models.ForeignKey(Iteration,related_name='removed')
+    desc = models.CharField(max_length=300)
+    status = models.IntegerField
+
+
+class Timer(models.Model):
+    totalTime = models.DurationField
+    session = models.OneToOneField(Session)
+
 
