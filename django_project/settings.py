2e5bedfc527d284bc9becd4f2aac60613230064b
Yang Zhifei <xividyzf@gmail.com>
2015-11-16 01:15:50 +0800

    for running on server

--- django_project/settings.py
+++ django_project/settings.py
@@ -56,23 +56,23 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+       'NAME': 'django',                      # Or path to database file if using sqlite3.
+       # The following settings are not used with sqlite3:
+       'USER': 'django',
+       'PASSWORD': 'pCXgUQjORe',
+       'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+       'PORT': '',                      # Set to empty string for default.
+   }
 }
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME': 'django',                      # Or path to database file if using sqlite3.
-#        # The following settings are not used with sqlite3:
-#        'USER': 'django',
-#        'PASSWORD': 'pCXgUQjORe',
-#        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-#        'PORT': '',                      # Set to empty string for default.
-#    }
-#}
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
